ceb818ab65cce1f1b1d6c8cdf9c4d70f00a71150
Ignacio Martinez <dark_nacho_xd@hotmail.cl>
2024-05-06 08:14:44 +0000

    VSCode DevContainer + New Authorization Rules

--- src/main/java/ca/uhn/fhir/jpa/starter/JWTSecurityInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/JWTSecurityInterceptor.java
@@ -1,68 +1,79 @@
 package ca.uhn.fhir.jpa.starter;
 
-
 import ca.uhn.fhir.rest.server.interceptor.auth.RuleBuilder;
-import ca.uhn.fhir.rest.server.interceptor.auth.IAuthRuleBuilder;
 import ca.uhn.fhir.rest.server.interceptor.auth.IAuthRule;
-import org.hl7.fhir.instance.model.api.IIdType;
-import org.hl7.fhir.r4.model.IdType;
-
-
-import ca.uhn.fhir.interceptor.api.Hook;
-import ca.uhn.fhir.interceptor.api.Interceptor;
+import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.client.interceptor.BearerTokenAuthInterceptor;
+import ca.uhn.fhir.rest.gclient.TokenClientParam;
 import ca.uhn.fhir.rest.server.exceptions.AuthenticationException;
 import ca.uhn.fhir.rest.server.interceptor.auth.AuthorizationInterceptor;
-import ca.uhn.fhir.interceptor.api.Pointcut;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 // JWT imports
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 import java.security.Key;
 import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.security.SignatureException;
+import org.springframework.http.HttpMethod;
 import io.jsonwebtoken.ExpiredJwtException;
+
+import org.hl7.fhir.r4.model.Bundle;
 //---
+import org.hl7.fhir.r4.model.IdType;
+
 import org.hl7.fhir.r4.model.Patient;
-import org.hl7.fhir.r4.model.Practitioner;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 /**
- * This class is an implementation of the AuthorizationInterceptor class and provides JWT-based security for FHIR requests.
- * It intercepts incoming requests and applies authorization rules based on the JWT token provided in the Authorization header.
- * The class verifies the token, extracts the user role and ID from the token claims, and builds the authorization rules accordingly.
+ * This class is an implementation of the AuthorizationInterceptor class and
+ * provides JWT-based security for FHIR requests.
+ * It intercepts incoming requests and applies authorization rules based on the
+ * JWT token provided in the Authorization header.
+ * The class verifies the token, extracts the user role and ID from the token
+ * claims, and builds the authorization rules accordingly.
  * If the token is invalid or expired, an AuthenticationException is thrown.
  * If the user role is unauthorized, an AuthenticationException is thrown.
  * If the request path is unauthenticated, a set of default rules is applied.
  */
 public class JWTSecurityInterceptor extends AuthorizationInterceptor {
 
-	private static final String SECRET_KEY = System.getenv("SECRET_KEY");
-	private static final Key DECODED_SECRET_KEY = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
-
+    private static final String SECRET_KEY = System.getenv("SECRET_KEY");
+    private static final Key DECODED_SECRET_KEY = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
 
     @Override
     public List<IAuthRule> buildRuleList(RequestDetails theRequestDetails) {
         String baseUrl = theRequestDetails.getFhirServerBase();
         String requestPath = theRequestDetails.getRequestPath();
+        String requestType = theRequestDetails.getRequestType().toString();
 
         System.out.println("baseUrl: " + baseUrl);
         System.out.println("requestPath: " + requestPath);
+        System.out.println("requestType: " + requestType);
 
-        if(isUnauthenticatedPath(requestPath))
+        if (isUnauthenticatedPath(requestPath))
             return new RuleBuilder().allowAll().build();
-        
-        if (requestPath.startsWith("Practitioner")) // For Practitioner resource, allow read access to the resource and deny all other requests
-            return new RuleBuilder().allow().read().allResources().withAnyId().andThen().denyAll().build();        
+
+        if (requestPath.startsWith("Practitioner") && requestType.equals("GET")) // Allow read all Practitioners
+            return new RuleBuilder().allow().read().allResources().withAnyId().andThen().denyAll().build();
 
         String authHeader = theRequestDetails.getHeader("Authorization");
-        if (authHeader == null) 
+        if (authHeader == null)
             throw new AuthenticationException("Must provide Authorization");
-        
+
         String jwtToken = authHeader.substring(7); // Remove "Bearer "
         System.out.println("Token: " + jwtToken);
 
@@ -78,7 +89,7 @@ public class JWTSecurityInterceptor extends AuthorizationInterceptor {
             return buildRulesBasedOnUserRole(userRole, userId, theRequestDetails);
         } catch (ExpiredJwtException e) {
             throw new AuthenticationException("Token is expired");
-        }catch (SignatureException e){ 
+        } catch (SignatureException e) {
             throw new AuthenticationException("Invalid token");
         } catch (Exception e) {
             throw new AuthenticationException("Authentication failed: " + e.getMessage());
@@ -89,31 +100,33 @@ public class JWTSecurityInterceptor extends AuthorizationInterceptor {
      * Checks whether a request path is in the list of unauthenticated paths.
      *
      * @param requestPath The HTTP request path to check.
-     * @return True if the request path is in the list of unauthenticated paths, false otherwise.
+     * @return True if the request path is in the list of unauthenticated paths,
+     *         false otherwise.
      *
-     * The unauthenticated paths include:
-     * - "swagger-ui/": The Swagger UI for API documentation.
-     * - "api-docs": The automatically generated API documentation.
-     * - "$get-resource-counts": A FHIR operation to get resource counts.
-     * - "metadata": The FHIR operation to get server metadata.
-     * - "$meta": A FHIR operation to get a resource's metadata.
-     * - "_history": A FHIR operation to get a resource's version history.
-     * - "Practitioner": The FHIR Practitioner resource.
+     *         The unauthenticated paths include:
+     *         - "swagger-ui/": The Swagger UI for API documentation.
+     *         - "api-docs": The automatically generated API documentation.
+     *         - "$get-resource-counts": A FHIR operation to get resource counts.
+     *         - "metadata": The FHIR operation to get server metadata.
+     *         - "$meta": A FHIR operation to get a resource's metadata.
+     *         - "_history": A FHIR operation to get a resource's version history.
+     *         - "Practitioner": The FHIR Practitioner resource.
      */
     private boolean isUnauthenticatedPath(String requestPath) {
-        return requestPath.equals("swagger-ui/") ||  
-            requestPath.startsWith("api-docs") ||
-            requestPath.equals("$get-resource-counts") ||
-            requestPath.equals("metadata") ||
-            requestPath.equals("$meta") ||
-            requestPath.equals("_history");
+        return requestPath.equals("swagger-ui/") ||
+                requestPath.startsWith("api-docs") ||
+                requestPath.equals("$get-resource-counts") ||
+                requestPath.equals("metadata") ||
+                requestPath.equals("$meta") ||
+                requestPath.equals("_history");
     }
 
     private Jws<Claims> parseJwtToken(String jwtToken) {
         return Jwts.parserBuilder().setSigningKey(DECODED_SECRET_KEY).build().parseClaimsJws(jwtToken);
     }
 
-    private List<IAuthRule> buildRulesBasedOnUserRole(String userRole, String userId, RequestDetails theRequestDetails) {
+    private List<IAuthRule> buildRulesBasedOnUserRole(String userRole, String userId,
+            RequestDetails theRequestDetails) {
         if ("Admin".equals(userRole)) {
             return new RuleBuilder().allowAll().build();
         } else if ("Patient".equals(userRole)) {
@@ -127,47 +140,65 @@ public class JWTSecurityInterceptor extends AuthorizationInterceptor {
 
     private List<IAuthRule> buildPatientRules(String userId) {
         return new RuleBuilder() // Allow the patient to read their own resources and deny all other requests
-            .allow()
-            .read()
-            .allResources()
-            .inCompartment("Patient", new IdType("Patient", userId))
-            .andThen()
-            .denyAll()
-            .build();
+                .allow("Read Patient Role").read().allResources()
+                .inCompartment("Patient", new IdType("Patient", userId))
+                .andThen()
+                .allow("Write Patient Role").write().allResources()
+                .inCompartment("Patient", new IdType("Patient", userId))
+                .andThen()
+                .denyAll("Deny all Patient Role")
+                .build();
     }
-
 
     private List<IAuthRule> buildPractitionerRules(String userId, RequestDetails theRequestDetails) {
-        if(theRequestDetails.getRequestPath().equals("Questionnaire"))
-            return new RuleBuilder().allowAll().build();
-        
-        
-        return new RuleBuilder()
-            .allow().read().resourcesOfType(Patient.class)
-            .inCompartment("Practitioner", new IdType("Practitioner", userId))
-            .andThen()
-            .allow().write().resourcesOfType(Patient.class)
-            .inCompartment("Practitioner", new IdType("Practitioner", userId))
-            .andThen()
-            .allow().create().resourcesOfType(Patient.class)
-            .inCompartment("Practitioner", new IdType("Practitioner", userId))
-            .andThen()
-            .allow().delete().resourcesOfType(Patient.class)
-            .inCompartment("Practitioner", new IdType("Practitioner", userId))
-            .andThen()
-            .allow().read().allResources()
-            .inCompartment("Practitioner", new IdType("Practitioner", userId))
-            .andThen()
-            .allow().write().allResources()
-            .inCompartment("Practitioner", new IdType("Practitioner", userId))
-            .andThen()
-            .allow().create().allResources()
-            .inCompartment("Practitioner", new IdType("Practitioner", userId))
-            .andThen()
-            .allow().delete().allResources()
-            .inCompartment("Practitioner", new IdType("Practitioner", userId))
-            .andThen()
-            .denyAll()
-            .build();
+        System.out.println("Buscando pacientes con general practitioner id: " + userId);
+        var patients = getPatientsWithPractitioner(theRequestDetails, userId);
+        System.out.println("Patients con practitioner id tamaño: " + patients.size());
+
+        RuleBuilder ruleBuilder = new RuleBuilder();
+        for (var patient : patients) {
+            System.out.println("Patient: " + patient.getIdPart());
+            ruleBuilder.allow().read().allResources().inCompartment("Patient",
+                    new IdType(patient.getResourceType(), patient.getIdPart())).andThen();
+            ruleBuilder.allow().write().allResources().inCompartment("Patient",
+                    new IdType(patient.getResourceType(), patient.getIdPart())).andThen();
+        }
+
+        ruleBuilder.allow().create().resourcesOfType("Patient").withAnyId().andThen();
+        ruleBuilder.denyAll("Deny all Practitioner Role");
+
+        return ruleBuilder.build();
     }
+
+    private List<IdType> getPatientsWithPractitioner(RequestDetails theRequestDetails, String practitionerId) {
+        // Create JWT token
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", "Admin");
+        String jwtToken = Jwts.builder()
+                .setClaims(claims)
+                .setExpiration(new Date(System.currentTimeMillis() + 60 * 1000)) // 1 minute
+                .signWith(DECODED_SECRET_KEY, SignatureAlgorithm.HS256)
+                .compact();
+
+        // Create FHIR context and client
+        FhirContext ctx = FhirContext.forR4();
+        IGenericClient client = ctx.newRestfulGenericClient(theRequestDetails.getFhirServerBase());
+
+        // Set the bearer token
+        client.registerInterceptor(new BearerTokenAuthInterceptor(jwtToken));
+
+        // Send request and parse result
+        Bundle response = client.search()
+                .forResource(Patient.class)
+                .where(new TokenClientParam("general-practitioner").exactly().code(practitionerId))
+                .returnBundle(Bundle.class)
+                .execute();
+
+        return response.getEntry().stream()
+                .map(Bundle.BundleEntryComponent::getResource)
+                .filter(resource -> resource instanceof Patient)
+                .map(resource -> ((Patient) resource).getIdElement())
+                .collect(Collectors.toList());
+    }
+
 }
